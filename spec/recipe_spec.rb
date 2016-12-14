06db83db53ffcae9f9a73c5cf5e44c82fb1ff3d1
Adam Zinder and Ezra Sandzer-Bell <ezra@>
2016-12-14 14:53:27 -0800

    passes rspec for cook time of recipe

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -6,5 +6,9 @@ describe(Recipe) do
       recipe = Recipe.create({:title => "Filet Mignon", :cook_time => 45, :serving_size => 5})
       expect(recipe.title).to(eq("Filet Mignon"))
     end
+    it('tests for cook time of recipe') do
+      recipe = Recipe.create({:title => "Filet Mignon", :cook_time => 45, :serving_size => 5})
+      expect(recipe.cook_time).to(eq(45))
+    end
   end
 end
