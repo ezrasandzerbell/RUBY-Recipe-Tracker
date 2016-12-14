9e5a453050aeefc004f71cf0df9e231b9a5e0c07
Adam Zinder and Ezra Sandzer-Bell <adam@>
2016-12-14 13:28:35 -0800

    add file and database archetecture

---
+++ db/migrate/20161214211605_tables.rb
@@ -0,0 +1,29 @@
+class Tables < ActiveRecord::Migration[5.0]
+  def change
+    create_table(:ingredients) do |t|
+      t.column(:name, :string)
+    end
+    create_table(:step_ingredients) do |t|
+      t.column(:quantity, :integer)
+      t.column(:measurement_type, :string)
+      t.column(:ingredient_id, :integer)
+      t.column(:step_id, :integer)
+    end
+    create_table(:steps) do |t|
+      t.column(:recipe_id, :integer)
+      t.column(:description, :string)
+    end
+    create_table(:recipes) do |t|
+      t.column(:title, :string)
+      t.column(:cook_time, :integer)
+      t.column(:serving_size, :integer)
+    end
+    create_table(:recipe_tags) do |t|
+      t.column(:recipe_id, :integer)
+      t.column(:tag_id, :integer)
+    end
+    create_table(:tags) do |t|
+      t.column(:type, :string)
+    end
+  end
+end
