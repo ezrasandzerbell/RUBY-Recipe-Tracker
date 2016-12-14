a4fcf14bdf772f220317ceeacb6ad0c5624f410b
Adam Zinder and Ezra Sandzer-Bell <ezra@>
2016-12-14 14:52:36 -0800

    create passing rspec for Recipe title

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -1,1 +1,10 @@
 require('spec_helper')
+
+describe(Recipe) do
+  describe('tests for recipe properties') do
+    it('tests for name of recipe') do
+      recipe = Recipe.create({:title => "Filet Mignon", :cook_time => 45, :serving_size => 5})
+      expect(recipe.title).to(eq("Filet Mignon"))
+    end
+  end
+end
