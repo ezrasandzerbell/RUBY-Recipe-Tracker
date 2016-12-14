fdea53b659016c6045de2dad5fb142d0b01200a8
Adam Zinder and Ezra Sandzer-Bell <adam@>
2016-12-14 14:09:11 -0800

    create passing rspec for step class, description method works

--- spec/step_spec.rb
+++ spec/step_spec.rb
@@ -1,4 +1,10 @@
-class Step
-  has_many(:step_ingredients)
-  belongs_to(:recipe)
+require('spec_helper')
+
+describe(Step) do
+  describe('tests step for properties') do
+    it('tests for description') do
+      step = Step.create({:description => "Preheat oven at 325 degrees"})
+      expect(step.description).to(eq("Preheat oven at 325 degrees"))
+    end
+  end
 end
