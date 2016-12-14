05e7f39b0f420e41fbb48ca3f3835752ebb497ef
Adam Zinder and Ezra Sandzer-Bell <ezra@>
2016-12-14 15:01:37 -0800

    pass rspec for integration between recipe spec and ingredients step

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -11,4 +11,16 @@ describe(Recipe) do
       expect(recipe.cook_time).to(eq(45))
     end
   end
+  describe('tests for integration with other tables') do
+    it('tests for integration with ingredients step') do
+      recipe = Recipe.create({:title => "Filet Mignon", :cook_time => 45, :serving_size => 5})
+      recipe_id = recipe.id
+      step = Step.create({:description => "Preheat oven at 325 degrees", :recipe_id => recipe_id})
+      step_id = step.id
+      ingredient = Ingredient.create({:name => "sugar"})
+      ingredient_id = ingredient.id
+      step_ingredient_test = IngredientsStep.create({:step_id => step_id, :ingredient_id => ingredient_id, :quantity => 4, :measurement_type => "oz"})
+      expect(recipe.steps).to(eq([step]))
+    end
+  end
 end
