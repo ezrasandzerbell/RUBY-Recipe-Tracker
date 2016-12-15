9920d47f33af7420fb7e0d9259c704490148b104
Adam Zinder <adam.zinder@gmail.com>
2016-12-15 14:19:46 -0800

    new front end integreations

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,4 +1,12 @@
 class Ingredient < ActiveRecord::Base
   has_many(:measurements)
   has_many(:recipes, through: :measurements)
+  before_save(:upcased_name)
+
+
+private
+
+  define_method(:upcased_name) do
+    self.name=(name().upcase())
+  end
 end
