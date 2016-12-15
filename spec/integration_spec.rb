d4f3aeb871807a7efd0c9525fc8b5feef1c01bf2
Ezra Sandzer-Bell <equilibriumarts@gmail.com>
2016-12-14 21:48:05 -0800

    pass int spec for nav to second page

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -6,5 +6,10 @@ describe('navigation') do
       visit('/')
       expect(page).to have_content("Hell's Kitchen")
     end
+    it('navigates to first page') do
+      visit('/')
+      click_link("Create new recipe")
+      expect(page).to have_content("Hell's Kitchen")
+    end
   end
 end
