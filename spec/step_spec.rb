45a9b5534d65b59c7b42852cb929aec170637825
Adam Zinder <adam.zinder@gmail.com>
2016-12-15 11:50:54 -0800

    passes all specs except for anamolyous integration spec

--- spec/step_spec.rb
+++ spec/step_spec.rb
@@ -3,8 +3,15 @@ require('spec_helper')
 describe(Step) do
   describe('tests step for properties') do
     it('tests for description') do
-      step = Step.create({:description => "Preheat oven at 325 degrees"})
-      expect(step.description).to(eq("Preheat oven at 325 degrees"))
+      step1 = Step.create({:description => "Preheat oven at 325 degrees"})
+      expect(step1.description).to(eq("Preheat oven at 325 degrees"))
+    end
+    it('tests the one to many relationship with recipes') do
+      recipe = Recipe.create({:title => "Filet Mignon", :cook_time => 45, :serving_size => 5})
+      recipe_id = recipe.id
+      step1 = Step.create({:description => "Preheat oven at 325 degrees", :recipe_id => recipe_id})
+      step2 = Step.create({:description => "Finely chop onions", :recipe_id => recipe_id})
+      expect(step1.recipe).to(eq(recipe))
     end
   end
 end
